c6909dce3f0d55eb030f2445ffae875dd4018224
KrauseStefan <stefan.krausekjaer@gmail.com>
2020-02-25 22:08:15 +0100

    initial commit of example app

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:togglTool/togglTool.dart' as togglTool;
+
+void main(List<String> arguments) {
+  print('Hello world: ${togglTool.calculate()}!');
+}
---
+++ test/togglTool_test.dart
@@ -0,0 +1,8 @@
+import 'package:togglTool/togglTool.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
